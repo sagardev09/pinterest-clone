e69d801a9d28685bc359b2ed3cd4a0c29dbb03e2
sagardev09 <sagardbs8121@gmail.com>
2023-12-17 22:57:53 +0530

    client id and secret changed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,13 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+console.log("Google Client ID:", process.env.NEXT_PUBLIC_NEXT_APP_CLIENT_ID);
+console.log("Google Client Secret:", process.env.NEXT_PUBLIC_NEXT_APP_CLIENT_SECRET);
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.NEXT_APP_CLIENT_ID,
-            clientSecret: process.env.NEXT_APP_CLIENT_SECRET
+            clientId: process.env.NEXT_PUBLIC_NEXT_APP_CLIENT_ID,
+            clientSecret: process.env.NEXT_PUBLIC_NEXT_APP_CLIENT_SECRET
         })
     ]
 })
