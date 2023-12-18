b6fb3ea073f3e9b4acb765a8e123399f68471557
sagardev09 <sagardbs8121@gmail.com>
2023-12-18 18:57:35 +0530

    minor updates

--- app/components/PinDetails/PinImage.js
+++ app/components/PinDetails/PinImage.js
@@ -3,16 +3,28 @@ import React from 'react'
 
 function PinImage({ pinDetail }) {
 
+    const isVideo = pinDetail.type == "video/mp4"
+
     return (
         <div>
-            <Image src={pinDetail.image}
-                alt={pinDetail.title}
-                width={1000}
-                height={1000}
-
-                className='rounded-2xl'
-            />
-
+            {isVideo ? (
+                <video
+                    src={pinDetail.image}
+                    width={1000}
+                    height={1000}
+                    className="rounded-2xl cursor-pointer relative z-0"
+                    autoPlay
+                    loop muted
+                ></video>
+            ) : (
+                <Image
+                    src={pinDetail.image}
+                    alt={pinDetail.title}
+                    width={1000}
+                    height={1000}
+                    className="rounded-2xl cursor-pointer relative z-0"
+                />
+            )}
         </div>
     )
 }
--- app/pin/[pinId]/page.js
+++ app/pin/[pinId]/page.js
@@ -6,27 +6,42 @@ import { HiArrowSmallLeft } from "react-icons/hi2";
 import { useRouter } from 'next/navigation'
 import PinImage from '@/app/components/PinDetails/PinImage';
 import PinInfo from '@/app/components/PinDetails/PinInfo';
+import Image from 'next/image';
+import Loading from "@/public/Ghost.gif"
 
 function PinDetail({ params }) {
     const router = useRouter();
     const db = getFirestore(app);
+    const [loading, setloading] = useState(true)
     const [pinDetail, setPinDetail] = useState([]);
     useEffect(() => {
-        getPinDetail();
-    }, [])
+        params && getPinDetail();
+    }, [params])
     const getPinDetail = async () => {
-        const docRef = doc(db, 'pinterest-post', params.pinId);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            setPinDetail(docSnap.data())
-        } else {
+        try {
+            const docRef = doc(db, 'pinterest-post', params.pinId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setPinDetail(docSnap.data())
+            } else {
 
-            console.log("No such document!");
+                console.log("No such document!");
+            }
+        } catch (error) {
+            console.log("error", error);
+        } finally {
+            setloading(false)
         }
+
     }
     return (
         <>
-            {pinDetail ?
+            {!pinDetail || loading ? (
+                <div className='flex flex-col gap-3 h-full w-[100vw] items-center justify-center'>
+                    <Image src={Loading} alt='loading' className='h-[200px] w-[200px] object-contain' />
+                    <h5 className='text-lg font-semibold capitalize'>Loading...</h5>
+                </div>
+            ) : (
                 <div className=' bg-white p-3 md:p-12 rounded-2xl md:px-24 lg:px-36'>
                     <HiArrowSmallLeft className='text-[60px] font-bold ml-[-50px] 
        cursor-pointer hover:bg-gray-200 rounded-full p-2 '
@@ -34,13 +49,14 @@ function PinDetail({ params }) {
                     <div className='grid grid-cols-1 lg:grid-cols-2 md:gap-10 shadow-lg
       rounded-2xl p-3 md:p-7 lg:p-12 xl:pd-16 '
                     >
-
                         <PinImage pinDetail={pinDetail} />
                         <div className="">
                             <PinInfo pinDetail={pinDetail} />
                         </div>
                     </div>
-                </div> : null}
+                </div>
+            )
+            }
         </>
     )
 }
