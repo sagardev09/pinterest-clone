6de694d4501e3713ad4138a99d66bd6a6dc3e569
sagardev09 <sagardbs8121@gmail.com>
2023-12-18 12:37:51 +0530

    route handler modification

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 console.log("Google Client ID:", process.env.NEXT_PUBLIC_NEXT_APP_CLIENT_ID);
 console.log("Google Client Secret:", process.env.NEXT_PUBLIC_NEXT_APP_CLIENT_SECRET);
+console.log("nextauth Secret:", process.env.NEXTAUTH_SECRET);
 
 const handler = NextAuth({
     providers: [
@@ -9,7 +10,8 @@ const handler = NextAuth({
             clientId: process.env.NEXT_PUBLIC_NEXT_APP_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_NEXT_APP_CLIENT_SECRET
         })
-    ]
+    ],
+    secret: process.env.NEXTAUTH_SECRET
 })
 
 export { handler as GET, handler as POST }
